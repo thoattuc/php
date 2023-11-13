0c798cccf0666709305fce1d0da3eb173d792850
thoattuc <thoattuc.hd@gmail.com>
2023-11-14 05:41:11 +0700

    commit

--- app_1/resources/views/layouts/layout.blade.php
+++ app_1/resources/views/layouts/layout.blade.php
@@ -1,6 +1,3 @@
-<?php
-    //
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,7 +9,43 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <link rel="stylesheet" href="./css/style.css">
+    <!-- Jquery CDN-->
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <style>
+        .sidebar {
+            position: fixed;
+            width: 250px;
+            height: 100%;
+            background: #333;
+            color: #fff;
+            padding-top: 20px;
+        }
+
+        .sidebar a {
+            padding: 10px 20px;
+            display: block;
+            color: #fff;
+        }
+
+        .sidebar a:hover {
+            background: #555;
+            text-decoration: none;
+        }
+
+        .content {
+            margin-left: 250px;
+            padding: 20px;
+        }
+
+        .border-none {
+            border: none;
+            border-radius: unset;
+        }
+
+        .account-icon {
+            color: white; /* Đổi màu icon thành màu trắng */
+        }
+    </style>
 </head>
 
 <body>
@@ -21,10 +54,14 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark row">
         <a class="navbar-brand col-md-2" href="#">Niam Dashboard</a>
 
-        <div class="input-group">
+        <div class="input-group ">
             <input type="text" class="border-none" placeholder="Tìm kiếm" aria-label="Tìm kiếm" aria-describedby="searchIcon">
             <button class="input-group-append btn input-group-text border-none" id="searchIcon"><i class="fas fa-search"></i></button>
         </div>
+
+        <button class="btn btn-primary border-none">
+            @yield('btn')
+        </button>
 
         <button class="btn btn-danger col-md-1 border-none">
             <i class="fas fa-user account-icon"></i>
@@ -41,11 +78,15 @@
         <div class="col-md-10 content">
             <!-- Content Area -->
             <h2>Nội dung sẽ được hiển thị ở đây</h2>
+            @yield('content')
         </div>
     </div>
 </div>
 
 <!-- Bootstrap JS and dependencies -->
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script>
+
+</script>
 </body>
 </html>
