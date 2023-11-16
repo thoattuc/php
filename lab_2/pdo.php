8d3caa648e21d28c337d181253741c7a3a09aa5f
thoattuc <thoattuc.hd@gmail.com>
2023-11-16 13:56:57 +0700

    commit

--- lab_2/pdo.php
+++ lab_2/pdo.php
@@ -3,7 +3,7 @@
  * Mở kết nối đến CSDL sử dụng PDO
  */
 function pdo_get_connection(){
-    $dburl = "mysql:host=localhost;dbname=db_lab;charset=utf8";
+    $dburl = "mysql:host=localhost;dbname=lab1;charset=utf8";
     $username = 'root';
     $password = '';
 
