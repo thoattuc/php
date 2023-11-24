21958e7921b855d3eb9ae43e9ec5dd46ec959dd5
thoattuc <thoattuc.hd@gmail.com>
2023-11-25 01:35:49 +0700

    commit

--- index.php
+++ index.php
@@ -1,23 +1,3 @@
 <?php
 
-echo "Hello World";
-
-// $to_email = "thoattuc.hd@gmail.com";
-
-// $subject = "Simple Email Testing via PHP";
-
-// $body = "Hello,nn It is a testing email sent by PHP Script";
-
-// $headers = "From: sender\'s email";
-
-
-
-// if (mail($to_email, $subject, $body, $headers)) {
-
-//     echo "Email successfully sent to $to_email...";
-
-// } else {
-
-//     echo "Email sending failed...";
-
-// }
+echo "Hello World!";
