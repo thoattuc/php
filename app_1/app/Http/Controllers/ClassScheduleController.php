fed3473c862333de418273af4610b7619cded71d
thoattuc <thoattuc.hd@gmail.com>
2023-11-28 17:45:23 +0700

    commit

---
+++ app_1/app/Http/Controllers/ClassScheduleController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ClassScheduleModel;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ClassScheduleController extends Controller
+{
+    public static function where(string $string, mixed $id)
+    {
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ClassScheduleModel $classScheduleModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ClassScheduleModel $classScheduleModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ClassScheduleModel $classScheduleModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ClassScheduleModel $classScheduleModel)
+    {
+        //
+    }
+}
---
+++ app_1/app/Http/Controllers/ProcessController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProcessModel;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProcessController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProcessModel $processModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProcessModel $processModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ProcessModel $processModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProcessModel $processModel)
+    {
+        //
+    }
+}
